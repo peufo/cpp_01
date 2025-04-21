27bbb790e882f060711e64e98bfe9d7b25674184
jonas <jonas.voisard@gmail.com>
2025-04-21 20:43:50 +0200

    setup dev env

---
+++ ex00/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "ZOOMBII" << std::endl;
+	return 0;
+}
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::cout << "YO" << std::endl;
+	return 0;
+}
