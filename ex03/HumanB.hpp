0e9db6e4ea7b5e866a316edfd80ab2350c797564
jonas <jonas.voisard@gmail.com>
2025-04-23 22:07:48 +0200

    ex03 ok

---
+++ ex03/HumanB.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include <string>
+#include <iostream>
+#include "Weapon.hpp"
+
+class HumanB
+{
+	private:
+		std::string name;
+		Weapon *weapon;
+	public:
+		HumanB(std::string name);
+		~HumanB();
+		void attack();
+		void setWeapon(Weapon &weapon);
+};
---
+++ ex03/Weapon.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include <string>
+
+class Weapon
+{
+	private:
+		std::string type;
+	public:
+		Weapon();
+		Weapon(std::string type);
+		~Weapon();
+		std::string &getType();
+		void setType(std::string type);
+};
