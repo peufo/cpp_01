5bc20da91a2de8967fe4b6c4c150641bb3610da6
jonas <jonas.voisard@gmail.com>
2025-04-24 15:11:51 +0200

    ex05 pretty log

--- ex05/Harl.cpp
+++ ex05/Harl.cpp
@@ -16,33 +16,35 @@ void Harl::complain(std::string level)
 {
 	Harl::log_func func = this->func_map[level];
 
-	std::cout << "[" << level << "]\t";
+	std::cout << "[" << level << "]" << std::endl;
 	if (func)
 		(this->*func)();
 	else
 		std::cout << "???" << std::endl;
+	std::cout << std::endl;
 }
 
 void Harl::debug(void)
 {
-	std::string msg = "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!";
-	std::cout << msg << std::endl;
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger." << std::endl;
+	std::cout << "I really do!" << std::endl;
 }
 
 void Harl::info(void)
 {
-	std::string msg = "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!";
-	std::cout << msg << std::endl;
+	std::cout << "I cannot believe adding extra bacon costs more money." << std::endl;
+	std::cout << "You didn’t put enough bacon in my burger!" << std::endl;
+	std::cout << "If you did, I wouldn’t be asking for more!" << std::endl;
 }
 
 void Harl::warning(void)
 {
-	std::string msg = "I think I deserve to have some extra bacon for free. I’ve been coming for years, whereas you started working here just last month.";
-	std::cout << msg << std::endl;
+	std::cout << "I think I deserve to have some extra bacon for free." << std::endl;
+	std::cout << "I’ve been coming for years, whereas you started working here just last month." << std::endl;
 }
 
 void Harl::error(void)
 {
-	std::string msg = "This is unacceptable! I want to speak to the manager now.";
-	std::cout << msg << std::endl;
+	std::cout << "This is unacceptable!" << std::endl;
+	std::cout << "I want to speak to the manager now." << std::endl;
 }
